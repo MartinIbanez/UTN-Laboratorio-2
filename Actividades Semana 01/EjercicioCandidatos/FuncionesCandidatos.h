dd156bae92069dfb3956f096b7569f5e65388c32
Martin Ibañez <107942533+MartinIbanez@users.noreply.github.com>
2023-08-13 02:09:10 -0300

    FuncionesCandidatos.h

---
+++ Actividades Semana 01/EjercicioCandidatos/FuncionesCandidatos.h
@@ -0,0 +1,8 @@
+#ifndef FUNCIONESCANDIDATOS_H_INCLUDED
+#define FUNCIONESCANDIDATOS_H_INCLUDED
+
+void cargarVotos(int *vecCandidatos,int *vecMunicipios);
+void puntoA(int *vecCandidatos,int tam);
+void puntoB(int vecMunicipios[], int tam);
+
+#endif // FUNCIONESCANDIDATOS_H_INCLUDED
